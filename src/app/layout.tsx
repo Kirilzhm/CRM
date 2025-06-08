06d234be4324d66e1ee7bfdaa4ecb24ac8dfcae9
Kirill <kirilzhm@gmail.com>
2025-06-08 13:00:39 +0200

    added a few files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './globals.css';
 import { Plus_Jakarta_Sans } from 'next/font/google'
+import Providers from '@/app/components/providers';
 
 const font  = Plus_Jakarta_Sans({subsets:['latin']});
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+                                        children,
+                                    }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
             <body className={font.className}>
-                {children}
-            </body>
+        <Providers>{children}</Providers>
+        </body>
         </html>
     );
 }
