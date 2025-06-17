fb018b86ba32e75bdf3c2ea0fa3c576737c8f3bf
Kirill <kirilzhm@gmail.com>
2025-06-17 12:18:54 +0200

    changes

--- src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
+++ src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
@@ -1,20 +1,16 @@
 'use client';
 
-import React from 'react';
 import { useRouter } from 'next/navigation';
 import PromotionFormModal from '@/app/components/promotion-form-modal';
 
-export interface PageProps {
-    params: { id: string };
-}
-
-export default function Page({ params }: PageProps) {
+export default function Page({ params }: { params: { id: string } }) {
     const router = useRouter();
+
     return (
-    <PromotionFormModal
-        companyId={params.id}
-        show={true}
-        onClose={() => router.back()}
-    />
+        <PromotionFormModal
+            companyId={params.id}
+            show={true}
+            onClose={() => router.back()}
+        />
     );
 }
